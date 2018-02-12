722926f7cce2d26484ae6357ea4c32972dca7e2b
Dave Laing <dave.laing.80@gmail.com>
2018-02-12 12:57:54 +1000

    Bumps reflex-platform

--- default.nix
+++ default.nix
@@ -1,5 +1,13 @@
-{ reflex-platform ? import ./reflex-platform.nix } :
+{ reflex-platform ? import ./nix/reflex-platform.nix } :
 let
-  reflex-basic-host = reflex-platform.ghc.callPackage ./reflex-basic-host.nix {};
+
+  haskellPackages = reflex-platform.ghc.override {
+    overrides = self: super: {
+      ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+      ghcWithPackages = self.ghc.withPackages;
+    };
+  };
+
+  reflex-basic-host = haskellPackages.callPackage ./reflex-basic-host.nix {};
 in
   reflex-basic-host
--- nix/reflex-platform.nix
+++ nix/reflex-platform.nix
@@ -1,12 +1,12 @@
 let  
   initialNixpkgs = import <nixpkgs> {};
 
-  sources = {
+  sources = rec {
+    reflex-platform-info-pinned = initialNixpkgs.pkgs.lib.importJSON ./reflex-platform.json;
     reflex-platform = initialNixpkgs.pkgs.fetchFromGitHub {
       owner = "reflex-frp";
       repo = "reflex-platform";
-      rev = "5fbbe11edd3674585d7b569cc218971a87b2f084";
-      sha256 = "0176jxwx7p8j3y6w1qwhnfdnr69d61acvm4bqbzhrc0h8fmbcnc7";
+      inherit (reflex-platform-info-pinned) rev sha256;
     };
   };
 
