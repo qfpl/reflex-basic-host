3ba892320a3fb917aa8772945fab4099b00a8fa4
Dave Laing <dave.laing.80@gmail.com>
2018-02-12 12:57:54 +1000

    Bumps reflex-platform

--- reflex-platform.nix
+++ reflex-platform.nix
@@ -5,8 +5,8 @@ let
     reflex-platform = initialNixpkgs.pkgs.fetchFromGitHub {
       owner = "reflex-frp";
       repo = "reflex-platform";
-      rev = "6aa1820d8d9ecae6744cd3f23f948a40a34f49c8";
-      sha256 = "1b9zz9kxfmrva1jz7i6mn7kgmlx5d13aqx0izsw296bhhcf348g7";
+      rev = "5fbbe11edd3674585d7b569cc218971a87b2f084";
+      sha256 = "0176jxwx7p8j3y6w1qwhnfdnr69d61acvm4bqbzhrc0h8fmbcnc7";
     };
   };
 
